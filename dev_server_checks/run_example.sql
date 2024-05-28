54fe50079c1689bdbe5125ac93ed460ae1943854
Cameron Schilling <cam.schilling@gmail.com>
2024-05-28 16:24:59 -0400

    updated run script to remove declaration

--- dev_server_checks/run_example.sql
+++ dev_server_checks/run_example.sql
@@ -3,9 +3,6 @@ From within Azure Data Studio, `enable SQLCMD`, hit run.
 Add or remove scripts that should be run following the approach below:
 */
 
--- DECLARE @infohub varchar(15);
--- SET @infohub = 'infohub';
-
 :r dev_server_checks\existence_checks_views.sql
 :r dev_server_checks\existence_checks_tables.sql
 :r dev_server_checks\count_checks.sql
