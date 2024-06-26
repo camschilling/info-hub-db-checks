ec58e9a93a6a9ced678634b35769f7bb4ae662a1
Cameron Schilling <cam.schilling@gmail.com>
2024-06-26 17:53:29 -0400

    added views for trends by month and year

---
+++ data_academy_view_generation/edav_project_count_trended.sql
@@ -0,0 +1,75 @@
+/*
+This script generates a set of views that are used to trend EDAV Project Count data over time.
+These queries are tuned to compute the number of active projects as is indicated by the month or year 
+*/
+
+
+--create a lookup table of months of interest
+-- drop if exists
+IF (EXISTS(
+    SELECT * 
+    FROM INFORMATION_SCHEMA.TABLES 
+    WHERE TABLE_NAME = 'EDAVProjectCountMonthsLookup' 
+    AND TABLE_SCHEMA = 'infohub'
+))
+   DROP TABLE [infohub].[EDAVProjectCountMonthsLookup];
+GO
+-- loop and insert values into relevant dates
+CREATE TABLE infohub.EDAVProjectCountMonthsLookup (
+  dt DATE
+)
+DECLARE @dIncr DATE = '2020-01-01'
+DECLARE @dEnd DATE = '2025-01-01'
+
+WHILE ( @dIncr < @dEnd )
+BEGIN
+  INSERT INTO infohub.EDAVProjectCountMonthsLookup (dt) VALUES(@dIncr)
+  SELECT @dIncr = DATEADD(MONTH, 1, @dIncr )
+END
+
+
+--summarize by ProjectCount month
+IF OBJECT_ID('infohub.EDAVProjectsByMonthTrend', 'V') IS NOT NULL
+    DROP VIEW [infohub].[EDAVProjectsByMonthTrend]
+GO
+
+CREATE VIEW [infohub].[EDAVProjectsByMonthTrend]
+AS
+-- left join ProjectCount using first of month
+    WITH active_projects_by_month AS(
+        SELECT months.dt, projects.*
+        FROM infohub.EDAVProjectCountMonthsLookup AS months
+        LEFT JOIN infohub.ProjectCount AS projects
+        ON months.dt BETWEEN projects.ProjectCreatedDate AND COALESCE(projects.ProjectClosedDate, '2099-01-01')
+    )
+    SELECT dt AS mo, SUM(ProjectCount) AS active_project_count_sum
+    FROM active_projects_by_month
+    GROUP BY dt;
+GO
+
+-- summarize ProjectCount by year
+IF OBJECT_ID('infohub.EDAVProjectsByYearTrend', 'V') IS NOT NULL
+    DROP VIEW [infohub].[EDAVProjectsByYearTrend]
+GO
+
+CREATE VIEW [infohub].[EDAVProjectsByYearTrend]
+AS
+    WITH years AS(
+        SELECT DISTINCT YEAR(dt) AS yr 
+        FROM infohub.EDAVProjectCountMonthsLookup
+    )
+    , active_projects_by_year AS(
+        SELECT years.yr, projects.*
+        FROM years
+        LEFT JOIN infohub.ProjectCount AS projects
+        ON years.yr BETWEEN YEAR(projects.ProjectCreatedDate) AND COALESCE(YEAR(projects.ProjectClosedDate), 2099)
+    )
+    SELECT yr, SUM(ProjectCount) AS project_count_sum
+    FROM active_projects_by_year
+    GROUP BY yr;
+GO
+
+
+
+SELECT * FROM [infohub].[EDAVProjectsByMonthTrend] ORDER BY mo;
+SELECT * FROM [infohub].[EDAVProjectsByYearTrend] ORDER BY yr;
