8ac24e5ca70951c5fc68bb825d9f9eb359d9053b
Cameron Schilling <cam.schilling@gmail.com>
2024-09-13 16:39:04 -0400

    finalized underlying pipelines and data merging logic

--- jurisdiction_view/jurisdiction_data_pull_240809.sql
+++ jurisdiction_view/jurisdiction_data_pull_240809.sql
@@ -4,73 +4,68 @@ This file creates a data structure that pulls together jurisdiction-level data f
 Ticket: https://jiradc.cdc.gov/browse/DIHM-2094
 
 PHDS Milestones:
-- eCR in surveillance systems: [infohub].[PHA eICR] -- last updated 6/27/24   DefinitiveHospitals.06.27.24.xlsx
-- intermediary connection: [infohub].[LDX Intermediaries] -- last updated 7/1 LDX PHDS Milestone Quarterly Data.xlsx (sharepoint.com)
-- ETOR PHL implementation: [infohub].[LDX ETOR] -- Confirm last update 7/12   LDX PHDS Milestone Quarterly Data.xlsx
-- electronic lab reporting? state level: (is this the NNDSS one?)
-- FHIR Adoption for mortality data : [infohub].[Mortality FHIR] -- UPDATE ME (last updated 3/12/24)   Other_PHDS_Metrics.xlsx
-- eCR expansion for reportable conditions
-- Wastewater
-- NHSN Automated data feeds
-- eCR for critical access hospitals (state level): [infohub].[eCR Hospitals] -- last updated 6/27/24   DefinitiveHospitals.06.27.24.xlsx
-- Core DUA
+- eCR in surveillance systems: [infohub].[PHA eICR] -- last updated 8/24 
+- intermediary connections: [infohub].[LDX Intermediaries] -- last updated 7/24 
+- ETOR PHL implementation: [infohub].[LDX ETOR] -- Confirm last update 7/24
+- electronic lab reporting? state level: NO DETAIL
+- FHIR Adoption for mortality data : [infohub].[Mortality FHIR] -- last updated 9/24
+- eCR expansion for reportable conditions: [infohub].[eCR_Reportable_Conditions] -- last updated 8/24
+- Wastewater: NO DETAIL
+- NHSN Automated data feeds: [infohub].[automated_hospital_capacity] -- pending confirmation of 8/24 updates
+- eCR for critical access hospitals (state level): [infohub].[eCR Hospitals] -- last updated 8/24
+- Core DUA: [infohub].[core_dua] -- pending confirmation of 8/24 updates
  
-
 DMI Priorities:
-- eCR condition-specific processing
-
-Other:
-Which does this map to? [infohub].[NNDSS GenV2] AS nndss -- UPDATE ME (last updated 4/3/24)   NNDSS
+- eCR condition-specific processing: [infohub].[eCR_Condition_Specific_Elements] -- last updated 8/24
 */
 
+IF OBJECT_ID('infohub.jurisdiction_view', 'V') IS NOT NULL
+    DROP VIEW [infohub].[jurisdiction_view]
+GO
 
-
-WITH prep AS(
+CREATE VIEW [infohub].[jurisdiction_view]
+AS
 SELECT lookup.*
-    , etor.[SPHL has implemented ETOR July 2023] AS ETOR_implemented
-    --, etor.Jurisdiction AS etor_jurisdiction 
-    , CASE WHEN etor.Jurisdiction IS NOT NULL THEN 1 ELSE 0 END AS etor_reported
-    , intermediaries.[AIMS]
-    , intermediaries.[ReportStream]
-    , intermediaries.[HIE**]
-    --, intermediaries.Jurisdiction AS intermediaries_jurisdiction
+    , etor.[SPHL has implemented ETOR July 2023] AS SPHL_ETOR_implemented
+    , CASE WHEN etor.Jurisdiction IS NOT NULL THEN 1 ELSE 0 END AS SPHL_ETOR_reported
+    , intermediaries.[AIMS] AS connected_AIMS
+    , intermediaries.[ReportStream] AS connected_ReportStream
+    , intermediaries.[HIE**] AS connected_HIE
     , CASE WHEN intermediaries.Jurisdiction IS NOT NULL THEN 1 ELSE 0 END AS intermediaries_reported
-    , mortality_fhir.[Status] AS mortality_fhir_status
-    --, mortality_fhir.Jurisdiction AS mortlaity_fhir_jurisdiction
+    , mortality_fhir.[Status] AS mortality_data_using_fhir
     , CASE WHEN mortality_fhir.Jurisdiction IS NOT NULL THEN 1 ELSE 0 END AS mortality_fhir_reported
-    , nndss.[MMG_implementation] AS nndss_mmg_implementation
-    , nndss.[GenV2_Content] AS nndss_genV2_content
-    , nndss.[MMG_implementation] AS nndss_genV2_new_condition
-    --, nndss.Jurisdiction AS nndss_jurisdiction
-    , CASE WHEN nndss.Jurisdiction IS NOT NULL THEN 1 ELSE 0 END AS nndss_reported
-    , pha_eicr.[processing] AS pha_eicr_processing
-    --, pha_eicr.[Jurisdiction (AB)] AS pha_eicr_jurisdiction
-    , CASE WHEN pha_eicr.[Jurisdiction (AB)] IS NOT NULL THEN 1 ELSE 0 END AS pha_eicr_reported
-    , ecr_hospitals.[# CAH] as num_CAH_in_state
-    , ecr_hospitals.[# CAH using eCR] as num_CAH_in_state_w_ecr
-    , ecr_hospitals.[% CAH using eCR]*100 pct_CAH_in_state_w_ecr
-    --, ecr_hospitals.[State] AS ecr_hospital_state
+    , pha_eicr.[processing] AS ecr_processing_surveillance_systems
+    , CASE WHEN pha_eicr.[Jurisdiction (AB)] IS NOT NULL THEN 1 ELSE 0 END AS ecr_processing_surveillance_systems_reported
+    , ecr_hospitals.[# CAH] AS num_CAH_in_state
+    , ecr_hospitals.[# CAH using eCR] AS num_CAH_in_state_w_ecr
+    , ROUND(ecr_hospitals.[% CAH using eCR]*100, 2) AS pct_CAH_in_state_w_ecr
     , CASE WHEN ecr_hospitals.[State] IS NOT NULL THEN 1 ELSE 0 END AS ecr_hospitals_reported
+    , ROUND(ecr_reportable_conditions.[% Conditions Receiving*]*100, 2) AS ecr_reportable_conditions_pct
+    , CASE WHEN ecr_reportable_conditions.[Jurisdiction] IS NOT NULL THEN 1 ELSE 0 END AS ecr_reportable_conditions_reported
+    , ecr_condition_specific_elements.[Processing] AS ecr_condition_specific_elements_status
+    , CASE WHEN ecr_condition_specific_elements.[Jurisdiction (AB)] IS NOT NULL THEN 1 ELSE 0 END AS ecr_condition_specific_elements_reported
+    , core_dua.[core_dua_status]
+    , CASE WHEN core_dua.[Jurisdiction_Abbreviation] IS NOT NULL THEN 1 ELSE 0 END AS core_dua_reported
+    , automated_hospital_capacity.[automated_hospital_capacity_daily_feed_status]
+    , CASE WHEN automated_hospital_capacity.[Jurisdiction_Abbreviation] IS NOT NULL THEN 1 ELSE 0 END AS automated_hospital_capacity_reported
 FROM [infohub].[jurisdiction_lookup] AS lookup
-    FULL OUTER JOIN [infohub].[LDX ETOR] AS etor -- Confirm up to date (assume 7/12) https://cdc.sharepoint.com/:x:/r/teams/DataModernizationImplementationLeads/InfoHubApps/_layouts/15/Doc.aspx?sourcedoc=%7BF690BCF2-369A-4C42-A903-4B55094F0BFF%7D&file=LDX%20PHDS%20Milestone%20Quarterly%20Data.xlsx&action=default&mobileredirect=true
-        ON lookup.Jurisdiction_Abbreviation = etor.Jurisdiction
-    FULL OUTER JOIN [infohub].[LDX Intermediaries] AS intermediaries -- last updated 7/1
-        ON lookup.Jurisdiction_Abbreviation = intermediaries.Jurisdiction
-    FULL OUTER JOIN [infohub].[Mortality FHIR] AS mortality_fhir -- UPDATE ME (last updated 3/12/24) https://cdc.sharepoint.com/:x:/r/teams/DataModernizationImplementationLeads/InfoHubApps/_layouts/15/Doc.aspx?sourcedoc=%7BEBEB2162-30ED-45A1-B490-E7C32D8AAA34%7D&file=Other_PHDS_Metrics.xlsx&action=default&mobileredirect=true
-        ON lookup.Jurisdiction_Abbreviation = mortality_fhir.Jurisdiction
-    FULL OUTER JOIN [infohub].[NNDSS GenV2] AS nndss -- UPDATE ME (last updated 4/3/24) https://cdc.sharepoint.com/teams/DataModernizationImplementationLeads/InfoHubApps/Shared%20Documents/Forms/AllItems.aspx?viewpath=%2Fteams%2FDataModernizationImplementationLeads%2FInfoHubApps%2FShared%20Documents%2FForms%2FAllItems%2Easpx&id=%2Fteams%2FDataModernizationImplementationLeads%2FInfoHubApps%2FShared%20Documents%2FNNDSS&viewid=acc584f3%2D7bc3%2D4046%2Da0ab%2D1a5c2e4c06c6
-        ON lookup.Jurisdiction_Abbreviation = 
-            CASE WHEN nndss.Jurisdiction = 'FSM/FM' THEN 'FM'
-            WHEN nndss.Jurisdiction = 'RMI/MH' THEN 'MH'
-            WHEN nndss.Jurisdiction = 'USVI/VI' THEN 'VI'
-            WHEN nndss.Jurisdiction = 'CNMI/MP' THEN 'MP'
-            ELSE nndss.Jurisdiction
-            END
-    FULL OUTER JOIN [infohub].[PHA eICR] AS pha_eicr -- last updated 6/27/24
-        ON lookup.Jurisdiction_Abbreviation = pha_eicr.[Jurisdiction (AB)]
-    FULL OUTER JOIN [infohub].[eCR Hospitals] AS ecr_hospitals -- last updated 6/27/24
-        ON lookup.Jurisdiction_Abbreviation = ecr_hospitals.[State]
-)
-SELECT * FROM prep --WHERE [Jurisdiction] IS NULL
-
+    FULL OUTER JOIN [infohub].[LDX ETOR] AS etor 
+        ON lookup.[Jurisdiction_Abbreviation] = etor.Jurisdiction
+    FULL OUTER JOIN [infohub].[LDX Intermediaries] AS intermediaries 
+        ON lookup.[Jurisdiction_Abbreviation] = intermediaries.Jurisdiction
+    FULL OUTER JOIN [infohub].[Mortality FHIR] AS mortality_fhir 
+        ON lookup.[Jurisdiction_Abbreviation] = mortality_fhir.Jurisdiction
+    FULL OUTER JOIN [infohub].[PHA eICR] AS pha_eicr 
+        ON lookup.[Jurisdiction_Abbreviation] = pha_eicr.[Jurisdiction (AB)]
+    FULL OUTER JOIN [infohub].[eCR Hospitals] AS ecr_hospitals 
+        ON lookup.[Jurisdiction_Abbreviation] = ecr_hospitals.[State]
+    FULL OUTER JOIN [infohub].[eCR_Reportable_Conditions] as ecr_reportable_conditions
+        ON lookup.[Jurisdiction_Abbreviation] = ecr_reportable_conditions.[Jurisdiction]
+    FULL OUTER JOIN [infohub].[eCR_Condition_Specific_Elements] AS ecr_condition_specific_elements
+        ON lookup.[Jurisdiction_Abbreviation] = ecr_condition_specific_elements.[Jurisdiction (AB)]
+    FULL OUTER JOIN [infohub].[core_dua] AS core_dua
+        ON lookup.[Jurisdiction_Abbreviation] = core_dua.[Jurisdiction_Abbreviation]
+    FULL OUTER JOIN [infohub].[automated_hospital_capacity] AS automated_hospital_capacity
+        ON lookup.[Jurisdiction_Abbreviation] = automated_hospital_capacity.[Jurisdiction_Abbreviation]
+;
 -- need a DQ check that [Jurisdiction] is never null
